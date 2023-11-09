684dbfbe4c8e4658bf4c389d18b15918c112d140
Mihail <klimenko.ma.1982@gmail.com>
2023-11-09 13:22:50 +0000

    Added first step

---
+++ dijkstra.py
@@ -0,0 +1,25 @@
+python
+def dijkstra(graph, start):
+    # Инициализация словарей для хранения расстояний и предшественников
+    distances = {vertex: float('infinity') for vertex in graph}
+    predecessors = {vertex: None for vertex in graph}
+    distances[start] = 0
+ Итерируемся по вершинам и обновляем расстояния, если найден более короткий путь через текущую вершину.
+python
+    # Итерация по вершинам
+    while graph:
+        # Выбираем вершину с наименьшим расстоянием
+        current_vertex = min(distances, key=distances.get)
+
+        # Итерируемся по соседям текущей вершины
+        for neighbor, weight in graph[current_vertex].items():
+            potential_distance = distances[current_vertex] + weight
+            # Если найден более короткий путь, обновляем расстояние и предшественника
+            if potential_distance < distances[neighbor]:
+                distances[neighbor] = potential_distance
+                predecessors[neighbor] = current_vertex
+        
+        # Удаляем обработанную вершину из графа
+        del distances[current_vertex]
+
+    return distances, predecessors
